3f76040f3d22929fe0821edcb8285fb823cb6ffd
Hernan <hernan.r9317@gmail.com>
2021-10-14 19:08:46 -0300

    Updates

---
+++ precache-manifest.30f1fc78de4e71da96396a614acc9991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c49d770478fba84fc9c6c14ece5ced2",
+    "url": "/gif-app/index.html"
+  },
+  {
+    "revision": "33ca08fa2c6e3d4be9e3",
+    "url": "/gif-app/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/gif-app/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gif-app/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ca08fa2c6e3d4be9e3",
+    "url": "/gif-app/static/js/main.e3382673.chunk.js"
+  },
+  {
+    "revision": "7bc3f9703384a78081db",
+    "url": "/gif-app/static/js/runtime-main.13c8dc6b.js"
+  }
+]);
